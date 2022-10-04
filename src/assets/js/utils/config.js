219dc5bf76a653aa40c52686d918b18b5ada7c7e
Romain Westeel <romainwesteel@orange.fr>
2022-10-04 21:07:59 +0200

    tentative rss

--- src/assets/js/utils/config.js
+++ src/assets/js/utils/config.js
@@ -3,37 +3,32 @@
  * @license CC-BY-NC 4.0 - https://creativecommons.org/licenses/by-nc/4.0/
  */
 
-const pkg = require('../package.json');
-const fetch = require("node-fetch")
-let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
-
-let config = `${url}/launcher/config-launcher/config.json`;
-let news = `${url}/launcher/news-launcher/assets/php/news/GetNews.php`;
-
-class Config {
-    GetConfig() {
-        return new Promise((resolve, reject) => {
-            fetch(config).then(config => {
-                return resolve(config.json());
-            }).catch(error => {
-                return reject(error);
-            })
-        })
-    }
-
-    async GetNews() {
-        let rss = await fetch(news);
-        if (rss.status === 200) {
-            try {
-                let news = await rss.json();
-                return news;
-            } catch (error) {
-                return false;
-            }
-        } else {
-            return false;
-        }
+ const pkg = require('../package.json');
+ const fetch = require("node-fetch")
+ const convert = require("xml2js")
+ const Parse = new convert.Parser()
+ let url = pkg.user ? `${pkg.url}/${pkg.user}` : pkg.url
+ 
+ let config = `${url}/launcher/config-launcher/config.json`;
+ let news = `https://centralcorp.fr/api/rss`;
+ 
+ class Config {
+     GetConfig() {
+         return new Promise((resolve, reject) => {
+             fetch(config).then(config => {
+                 return resolve(config.json());
+             }).catch(error => {
+                 return reject(error);
+             })
+         })
+     }
+ 
+     async GetNews() {
+         let rss = await fetch(news).then(res => res.text()).catch(err => err)
+         Parse.parseString(rss, data=>{
+            console.log(data)
+         })
     }
 }
-
-export default new Config;
+ 
+ export default new Config;
